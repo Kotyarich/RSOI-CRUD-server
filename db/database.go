b4c42c65db8efeca900de404fcdc43c576345116
Kotyarich <ndkotov@gmail.com>
2021-09-12 05:39:51 +0300

    Db connection fixed, heroku url added

--- db/database.go
+++ db/database.go
@@ -11,7 +11,7 @@ func init() {
 	dsn := "host=ec2-54-74-14-109.eu-west-1.compute.amazonaws.com " +
 		"user=pmfbtvnvqzyukr " +
 		"password=e629f697b700dcaf54c9f1bf4c6ffe1318f2b8f5771461e46764238a160cf8c1 " +
-		"dbname=dd73bum723rkng port=5432 sslmode=disable TimeZone=Europe/Moscow"
+		"dbname=dd73bum723rkng port=5432 TimeZone=Europe/Moscow"
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
