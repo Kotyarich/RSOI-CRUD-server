f91e3c145699868a79e759fc71b373fbb8662f84
Kotyarich <ndkotov@gmail.com>
2021-09-12 17:11:12 +0300

    Reading port from env, heroku url

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 	"rsoi-2021-lab1-ci-cd-Kotyarich/server"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	app := server.NewApp()
 
-	if err := app.Run(":8080"); err != nil {
+	if err := app.Run(":" + port); err != nil {
 		log.Fatalf("%s", err.Error())
 	}
 }
